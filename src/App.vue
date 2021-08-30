4ada300ab9745afd65e6b06a1060886c015b5d08
NikarashiHatsu <yourlovelydev@gmail.com>
2021-08-31 01:11:14 +0700

    fix some thing

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
   <div style="backgroundColor: #ff2323" class="w-full h-screen flex items-center justify-center relative overflow-hidden">
     <div @click="touchs" class="w-full h-full absolute top-0 left-0 z-10"></div>
 
-    <h1 class="text-4xl md:text-6xl absolute top-0 text-white font-bold mt-8 tracking-wide">POPTOPIK</h1>
-    <h1 class="text-4xl md:text-6xl absolute top-0 text-white font-bold mt-20 md:mt-24 tracking-wide">{{ pops }}</h1>
+    <h1 class="text-5xl md:text-6xl absolute top-0 text-white font-bold mt-8 tracking-wide">POPTOPIK</h1>
+    <h1 class="text-5xl md:text-6xl absolute top-0 text-white font-bold mt-20 pt-2 md:pt-0 md:mt-24 tracking-wide">{{ pops }}</h1>
     <img src="./assets/images/banteng_loli_pdip.jpg" class="w-full sm:w-auto sm:h-full top-0 bottom-0" />
     <img :src="tempik" class="absolute bottom-0 w-full sm:w-auto" />
   </div>
 </template>
 
 <script>
-  import tempik1 from './assets/images/tempik1.png'
-  import tempik2 from './assets/images/tempik2.png'
+  import taufik1 from './assets/images/taufik1.png'
+  import taufik2 from './assets/images/taufik2.png'
   import uhh from './assets/sounds/uhh.mp3'
   import kaget from './assets/sounds/kaget.mp3'
 
@@ -52,8 +52,8 @@
     computed: {
       tempik() {
         return this.clicked 
-          ? tempik2
-          : tempik1
+          ? taufik2
+          : taufik1
       }
     }
   }
